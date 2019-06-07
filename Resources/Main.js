a9d1be0260ab1df6c2029dd55fa9bc96500debe5
Brian Pineau <pinoub@gmail.com>
2019-06-06 23:47:03 -0500

    removed border from skills section

--- Resources/Main.js
+++ Resources/Main.js
@@ -1,11 +1,29 @@
 
 /*
-function resizeHeader() {
-    document.getElementByTagName("header").style.height = 60;
-}
+function resizeHeaderOnScroll() {
+    const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+    shrinkOn = 200,
+    headerEl = document.getElementById("js-header");
+    
+    if (distanceY > shrinkOn) {
+      headerEl.classList.add("smaller");
+    } else {
+      headerEl.classList.remove("smaller");
+    }
+  }
+  
+  window.addEventListener('scroll', resizeHeaderOnScroll);
 */
 
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("js-title").style.fontSize = "30px";
+  } else {
+    document.getElementById("js-title").style.fontSize = "90px";
+  }
+}
 
 
 
